11330fd8175a03c28ec394d436bcfadc6263dc6c
Jean-Olivier Irisson <irisson@normalesup.org>
2010-07-27 17:17:41 +0200

    Improved simple ggplot theme
    For publications in journal that require blank figures

--- lib_ggplot.R
+++ lib_ggplot.R
@@ -45,7 +45,7 @@ theme_blight <- function(base_size = 12) {
   ), class = "options")
 }
 
-theme_white <- function(base_size = 12) {
+theme_gray <- function(base_size = 12) {
   structure(list(
     axis.line =         theme_blank(),
     axis.text.x =       theme_text(size = base_size * 0.8 , lineheight = 0.9, colour = "grey50", vjust = 1),
@@ -56,26 +56,61 @@ theme_white <- function(base_size = 12) {
     axis.ticks.length = unit(0.15, "cm"),
     axis.ticks.margin = unit(0.1, "cm"),
 
-    legend.background = theme_blank(),
+    legend.background = theme_rect(colour="white"), 
     legend.key =        theme_rect(fill = "grey95", colour = "white"),
     legend.key.size =   unit(1.2, "lines"),
     legend.text =       theme_text(size = base_size * 0.8),
     legend.title =      theme_text(size = base_size * 0.8, face = "bold", hjust = 0),
     legend.position =   "right",
 
-	panel.background =  theme_blank(),
-    panel.border =      theme_blank(),
-	panel.grid.major =  theme_blank(),
-	panel.grid.minor =  theme_blank(),
+    panel.background =  theme_rect(fill = "grey90", colour = NA), 
+    panel.border =      theme_blank(), 
+    panel.grid.major =  theme_line(colour = "white"),
+    panel.grid.minor =  theme_line(colour = "grey95", size = 0.25),
     panel.margin =      unit(0.25, "lines"),
 
-    strip.background =  theme_rect(beamer$defaultAddedBlue, colour = "white"),
+    strip.background =  theme_rect(fill = "grey80", colour = NA), 
     strip.text.x =      theme_text(size = base_size * 0.8),
     strip.text.y =      theme_text(size = base_size * 0.8, angle = -90),
 
     plot.background =   theme_rect(colour = NA, fill = "white"),
     plot.title =        theme_text(size = base_size * 1.2),
-    plot.margin =       unit(rep(0.2, 4), "lines")
+    plot.margin =       unit(c(1, 1, 0.5, 0.5), "lines")
+  ), class = "options")
+}
+
+
+theme_simple <- function(base_size = 12) {
+  structure(list(
+    axis.line =         theme_blank(),
+    axis.text.x =       theme_text(size = base_size * 0.8 , lineheight = 0.9, colour = "black", vjust = 1),
+    axis.text.y =       theme_text(size = base_size * 0.8, lineheight = 0.9, colour = "black", hjust = 1),
+    axis.ticks =        theme_segment(colour = "black"),
+    axis.title.x =      theme_text(size = base_size, vjust = 0.5),
+    axis.title.y =      theme_text(size = base_size, angle = 90, vjust = 0.5),
+    axis.ticks.length = unit(0.15, "cm"),
+    axis.ticks.margin = unit(0.1, "cm"),
+
+    legend.background = theme_blank(),
+    legend.key =        theme_blank(),
+    legend.key.size =   unit(1.2, "lines"),
+    legend.text =       theme_text(size = base_size * 0.8),
+    legend.title =      theme_text(size = base_size, face = "bold", hjust = 0),
+    legend.position =   "bottom",
+
+	panel.background =  theme_rect(fill=NA, colour = "black", size=0.5),
+    panel.border =      theme_blank(),
+	panel.grid.major =  theme_blank(),
+	panel.grid.minor =  theme_blank(),
+    panel.margin =      unit(0.25, "lines"),
+
+    strip.background =  theme_rect(fill="grey30", colour = NA),
+    strip.text.x =      theme_text(size = base_size * 0.8, colour="white"),
+    strip.text.y =      theme_text(size = base_size * 0.8, angle = -90, colour="white"),
+
+    plot.background =   theme_blank(),
+    plot.title =        theme_text(size = base_size * 1.2),
+    plot.margin =       unit(rep(0, 4), "lines")
   ), class = "options")
 }
 
